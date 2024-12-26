7ed1aaa168110d8e52d7defcc5553400b294c06e
boss160250@gmail.com <boss160250@gmail.com>
2024-12-26 19:24:39 +0700

    syntax highlighting

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+  opts ={ ensure_installed ={
+    "bash",
+    "yaml",
+    "vim",
+    "lua",
+    "json",
+    "c",
+    "javascript",
+    "html",
+    "typescript",
+    "tsx",
+    "css",
+    "jsonc",
+    "markdown",
+    "markdown_inline",
+    "python",
+    "go",
+    "css",
+    },
+  },
+},
+}
+
