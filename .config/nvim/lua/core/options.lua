3540a0fa8972a8134ac105b9fcb1dedf3ba78702
boss160250@gmail.com <boss160250@gmail.com>
2024-12-26 19:27:11 +0700

    options

---
+++ .config/nvim/lua/core/options.lua
@@ -0,0 +1,14 @@
+--vim.wo.number = true
+vim.o.mouse = 'a'
+--tabs & indentations
+vim.o.relativenumber = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth =2 
+vim.o.expandtab = true
+vim.o.backspace = "indent,eol,start"
+vim.o.autoindent = true
+vim.o.wrap = false
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.cursorline = true
