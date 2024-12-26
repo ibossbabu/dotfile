c1e9f723a4f959129a4e9a996c46467e0211b9ae
boss160250@gmail.com <boss160250@gmail.com>
2024-12-26 20:03:53 +0700

    desktop environment config

---
+++ .config/wezterm/boss.lua
@@ -0,0 +1,18 @@
+return {
+    foreground = "#ffffff",
+    background = "#16181a",
+
+    cursor_bg = "#0b8c3d",
+    cursor_fg = "#0b8c3d",
+    cursor_border = "#0b8c3d",
+
+    selection_fg = "#ffffff",
+    selection_bg = "#3c4048",
+
+    scrollbar_thumb = "#16181a",
+    split = "#16181a",
+
+    ansi = { "#16181a", "#ff6e5e", "#5eff6c", "#f1ff5e", "#5ea1ff", "#bd5eff", "#5ef1ff", "#ffffff" },
+    brights = { "#3c4048", "#ff6e5e", "#5eff6c", "#f1ff5e", "#5ea1ff", "#bd5eff", "#5ef1ff", "#ffffff" },
+    indexed = { [16] = "#ffbd5e", [17] = "#ff6e5e" },
+}
